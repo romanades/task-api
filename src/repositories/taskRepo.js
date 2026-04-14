2924e696911d6e4c34e19212a7d132f10096b251
Roman Ades <romanades314@gmail.com>
2026-04-14 18:31:36 +0200

    update task repo

--- src/repositories/taskRepo.js
+++ src/repositories/taskRepo.js
@@ -1,10 +1,10 @@
 import prisma from '../config/db.js';
 
-export async function findAll() {
-  return prisma.task.findMany();
+export async function findAll(completed) {
+  const where = completed === undefined ? {} : { completed };
+  return prisma.task.findMany({ where });
 }
 
-// Create a new task
 export async function create(data) {
   return prisma.task.create({
     data,
